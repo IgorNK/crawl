a90a7cbb1e9e19a51faf9cf469bfe84c5309876a
IgorNK <117646556+IgorNK@users.noreply.github.com>
2023-08-22 07:58:32 +0000

    Removed copy traits

--- src/model.rs
+++ src/model.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
-#[derive(Clone, Copy)]
+#[derive(Clone)]
 pub struct SystemData {
   pub races: Option<Vec<Race>>,
   pub classes: Option<Vec<Class>>,
@@ -64,7 +64,7 @@ impl<'a> PlayerCharacter<'a> {
 }
 
 #[derive(Deserialize, Serialize)]
-#[derive(Clone, Copy)]
+#[derive(Clone)]
 pub struct Race {
     pub name: String,
     pub attr_bonus: Attributes,
@@ -80,7 +80,7 @@ impl Default for Race {
 }
 
 #[derive(Deserialize, Serialize)]
-#[derive(Clone, Copy)]
+#[derive(Clone)]
 pub struct Class {
     pub name: String,
 }
